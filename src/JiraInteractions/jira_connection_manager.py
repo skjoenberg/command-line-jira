a04239098a9c7098b2f1f81764013cfb52cfdd13
sebastian <sebastian@skjoenberg.com>
2019-11-12 17:37:19 +0100

    Postphoned establishing jira connection to first usage

--- src/JiraInteractions/jira_connection_manager.py
+++ src/JiraInteractions/jira_connection_manager.py
@@ -10,38 +10,46 @@ from CommandLine.command_line_utilities import write_to_console
 from typing import Optional
 
 
+def _print_cannot_connect_error_message():
+    write_to_console(
+        "Jira connection returned: 401 Unauthorized error.\n" +
+        "The error is most likely causing by wrong login information. Try resetting your password"
+    )
+
+
 class JiraConnectionHandler:
-    _jira_agile: Optional[JIRA]
+    _jira_agile_connection: Optional[JIRA]
     _loginHandler: LoginManager
 
     def __init__(self, login_manager: LoginManager, config_manager: ConfigManager):
         self._loginHandler = login_manager
         self._server = config_manager.server
-        self._jira_agile = None
-        self._jira_greenhopper = None
+        self._jira_agile_connection = None
+        self._jira_greenhopper_connection = None
 
-        self._establish_connection()
+    def _jira_agile(self):
+        if self._jira_agile_connection is None:
+            try:
+                agile_parameters, _ = self._setup_connection_parameters()
+                jira_agile_connection: JIRA = JIRA(
+                    basic_auth=agile_parameters["basic_auth"],
+                    options=agile_parameters["options"])
+                self._jira_agile_connection = jira_agile_connection
+            except RecursionError as _:
+                _print_cannot_connect_error_message()
+        return self._jira_agile_connection
 
-    def _establish_connection(self):
-        try:
-            agile_parameters, greenhopper_parameters = self._setup_connection_parameters()
-
-            jira_agile_connection: JIRA = JIRA(
-                basic_auth=agile_parameters["basic_auth"],
-                options=agile_parameters["options"])
-
-            jira_greenhopper_connection: JIRA = JIRA(
-                basic_auth=greenhopper_parameters["basic_auth"],
-                options=greenhopper_parameters["options"])
-
-            self._jira_agile = jira_agile_connection
-            self._jira_greenhopper = jira_greenhopper_connection
-
-        except RecursionError as _:
-            write_to_console(
-                "Jira connection returned: 401 Unauthorized error.\n" +
-                "The error is most likely causing by wrong login information. Try resetting your password"
-            )
+    def _jira_greenhooper(self):
+        if self._jira_greenhopper_connection is None:
+            try:
+                _, greenhopper_parameters = self._setup_connection_parameters()
+                jira_greenhopper_connection: JIRA = JIRA(
+                    basic_auth=greenhopper_parameters["basic_auth"],
+                    options=greenhopper_parameters["options"])
+                self._jira_greenhopper_connection = jira_greenhopper_connection
+            except RecursionError as _:
+                _print_cannot_connect_error_message()
+        return self._jira_greenhopper_connection
 
     def _setup_connection_parameters(self):
         """Creates the parameters needed for establishing a connection to Jira"""
